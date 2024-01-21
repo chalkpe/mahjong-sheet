4b8277ff693d97637a0bfa3d3b486d5e26253b62
Chalk <9481405+ChalkPE@users.noreply.github.com>
2024-01-21 18:43:11 +0900

    feat: import and export

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css";
 
-import { Button, Card, Form, Segmented, Space } from "antd";
-import { GithubOutlined } from "@ant-design/icons";
+import { Button, Card, Form, Segmented, Space, message } from "antd";
+import { ExportOutlined, GithubOutlined, ImportOutlined } from "@ant-design/icons";
 import useLocalStorage from "use-local-storage";
 
 import NameEdit from "./components/NameEdit";
@@ -26,12 +26,45 @@ function App() {
       <Card
         title="기본 설정"
         extra={
-          <Button
-            icon={<GithubOutlined />}
-            onClick={() =>
-              open("https://github.com/chalkpe/mahjong-sheet", "_blank")
-            }
-          />
+          <Space>
+            <Button
+              icon={<ImportOutlined />}
+              onClick={() => {
+                navigator.clipboard.readText().then((text) => {
+                  try {
+                    const { mode, names, data } = JSON.parse(text);
+                    setMode(mode);
+                    setNames(names);
+                    setData(data);
+                    message.success("클립보드에서 데이터를 가져왔습니다.");
+                  } catch {
+                    message.error("클립보드에 올바른 데이터가 없습니다.");
+                  }
+                });
+              }}
+            >
+              가져오기
+            </Button>
+
+            <Button
+              icon={<ExportOutlined />}
+              onClick={() => {
+                navigator.clipboard.writeText(
+                  JSON.stringify({ mode, names, data })
+                );
+                message.success("클립보드에 데이터를 복사했습니다.");
+              }}
+            >
+              내보내기
+            </Button>
+
+            <Button
+              icon={<GithubOutlined />}
+              onClick={() =>
+                open("https://github.com/chalkpe/mahjong-sheet", "_blank")
+              }
+            />
+          </Space>
         }
       >
         <Form>
